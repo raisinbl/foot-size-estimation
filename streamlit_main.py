2192288a23dccea0ebb864eb46a59014cbd0c253
Duy Hung <66005831+raisinbl@users.noreply.github.com>
2022-07-01 01:06:43 +0700

    update web page

--- streamlit_main.py
+++ streamlit_main.py
@@ -24,62 +24,60 @@ def upload_image():
     return uploaded_image
 
 
-if __name__ == '__main__':
-    # Title
-    st.title('Feet size estimation')
+# Title
+st.title('Feet size estimation')
+fh, fw = 0, 0
+st.write('Please upload your feet image to estimate')
+img = upload_image()
+if img is not None:
+    is_process = st.button("Let's find your feet size")
+    if is_process:
+        wait = st.write('Your image is processing, Please wait!')
+        #prepocess
+        img_class = classifier(img)
+        preprocess_img = preprocess(img, img_class)
+        if preprocess_img is not None:
+            del wait
+        # segments of foot, paper, paper's corners...
 
-    img = upload_image()
-    if img is not None:
-        is_process = st.button("Let's find your feet size")
-        if is_process:
-            
-            #prepocess
-            img_class = classifier(img)
-            preprocess_img = preprocess(img, img_class)
-            
-            # segments of foot, paper, paper's corners...
-            st.title("Here is the image after preprocessing")
-            st.image(preprocess_img, caption='Preprocessed Image', use_column_width=True)
+        clustered_img = kMeans_cluster(preprocess_img)
+        
+        st.title("Here is the image after clustering")
+        st.image(clustered_img, caption='Clustered Image', use_column_width=True)
 
-            clustered_img = kMeans_cluster(preprocess_img)
-            
-            st.title("Here is the image after clustering")
-            st.image(clustered_img, caption='Clustered Image', use_column_width=True)
+        edge_detected_img = paperEdgeDetection(clustered_img)
+        
+        st.title("Here is the image after edge detection")
+        st.image(edge_detected_img, caption='Edge Detected Image', use_column_width=True)
 
-            edge_detected_img = paperEdgeDetection(clustered_img)
+        boundRect, contours, contours_poly, img = getBoundingBox(edge_detected_img)
+        
+        st.title("Here is the image after bounding box")
+        st.image(img, caption='Bounding Box Image', use_column_width=True)
+        
+        if img_class == 0:
+            cropped_img, pcropped_img = cropOrig(boundRect[0], clustered_img)
+        else:
+            cropped_img, pcropped_img = cropOrig(boundRect[1], clustered_img)
             
-            st.title("Here is the image after edge detection")
-            st.image(edge_detected_img, caption='Edge Detected Image', use_column_width=True)
+        st.title("Here is the image after cropping")
+        st.image(cropped_img, caption='Cropped Image', use_column_width=True)
 
-            boundRect, contours, contours_poly, img = getBoundingBox(edge_detected_img)
-            
-            st.title("Here is the image after bounding box")
-            st.image(img, caption='Bounding Box Image', use_column_width=True)
-            
-            if img_class == 0:
-                cropped_img, pcropped_img = cropOrig(boundRect[0], clustered_img)
-            else:
-                cropped_img, pcropped_img = cropOrig(boundRect[1], clustered_img)
-                
-            st.title("Here is the image after cropping")
-            st.image(cropped_img, caption='Cropped Image', use_column_width=True)
+        new_img = overlayImage(cropped_img, pcropped_img)
+        
+        st.title("Here is the image after overlay")
+        st.image(new_img, caption='Overlay Image', use_column_width=True)
 
-            new_img = overlayImage(cropped_img, pcropped_img)
-            
-            st.title("Here is the image after overlay")
-            st.image(new_img, caption='Overlay Image', use_column_width=True)
+        fedged = footEdgeDetection(new_img)
+        fboundRect, fcnt, fcntpoly, fimg = getBoundingBox(fedged)
+        fdraw = drawCnt(fboundRect[0], fcnt, fcntpoly, fimg)
+        
+        st.title("Here is the image after edge detection")
+        st.image(fdraw, caption='Edge Detected Image', use_column_width=True)
 
-            fedged = footEdgeDetection(new_img)
-            fboundRect, fcnt, fcntpoly, fimg = getBoundingBox(fedged)
-            fdraw = drawCnt(fboundRect[0], fcnt, fcntpoly, fimg)
-            
-            st.title("Here is the image after edge detection")
-            st.image(fdraw, caption='Edge Detected Image', use_column_width=True)
-
-            ofs, fh, fw, ph, pw = calcFeetSize(pcropped_img, fboundRect)
-            
-            # resized_image = og_img.resize((336, 336))
-            # st.image(resized_image)
-            st.title(f"[INFO] Feet size (cm): {ofs}")
-
-            
+        ofs, fh, fw, ph, pw = calcFeetSize(pcropped_img, fboundRect)
+        
+        st.title(f"[INFO] Feet size (cm): {ofs}")
+        st.write(f"[INFO] Width (cm): {fw}")
+        st.write(f"[INFO] Height(cm): {fh}")
+    
